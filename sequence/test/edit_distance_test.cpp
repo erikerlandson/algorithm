7146cd782063d3aef9852ddf160fc6a19fff2f4a
Erik Erlandson <eerlands@redhat.com>
2014-02-01 13:03:16 -0700

    get rid of macro redef warning

--- sequence/test/edit_distance_test.cpp
+++ sequence/test/edit_distance_test.cpp
@@ -10,6 +10,10 @@ See accompanying file LICENSE or copy at
 http://www.boost.org/LICENSE_1_0.txt
 *******/
 
+#if !defined(BOOST_TEST_DYN_LINK)
 #define BOOST_TEST_DYN_LINK
+#endif
+
 #define BOOST_TEST_MODULE edit_distance
+
 #include <boost/test/unit_test.hpp>
